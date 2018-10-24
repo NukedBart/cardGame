1ecc877c2d09d5823e1e800f262a14559dadab64
OwlSurojit <surojit@gmx.de>
2018-10-24 23:07:15 +0200

    Initial commit

---
+++ card.js
@@ -0,0 +1,67 @@
+var back = new Image();
+back.src = "cardBack.png";
+var stdW = 90;
+var stdH = 160;
+
+class Card{	
+	constructor(imgSrc, w, h){
+		Card.rsz();
+		this.img = new Image();
+		this.img.src = imgSrc;
+		this.img.loaded = false;
+	}
+	
+	static rsz(w, h){
+		stdW = w || (scl * 90);
+		stdH = h || (scl * 160);
+	}
+	
+	display(x, y, showBack = false){
+		let ctx = document.getElementById("canvas").getContext("2d");
+		if(showBack){
+			ctx.drawImage(back, x, y, stdW, stdH);
+			ctx.roundRect(x, y, stdW, stdH, Math.ceil(scl * 5));
+			ctx.stroke();
+		}else{
+			if(this.img.loaded){
+				ctx.drawImage(this.img, x, y, stdW, stdH);
+				ctx.roundRect(x, y, stdW, stdH);
+				ctx.stroke();
+			}else{
+				this.img.onload = () => {
+					console.log("ha!");
+					ctx.drawImage(this.img, x, y, stdW, stdH);
+					ctx.roundRect(x, y, stdW, stdH, Math.ceil(scl * 5));
+					ctx.stroke();
+					this.img.loaded = true;
+				}
+			}
+		}
+	}
+}
+
+
+/* Inspired by several code snippets from this stackoverflow entry:
+ * https://stackoverflow.com/questions/1255512/how-to-draw-a-rounded-rectangle-on-html-canvas
+ */
+CanvasRenderingContext2D.prototype.roundRect = function(x, y, width, height, radius = 5) {
+    if (typeof radius === 'number') {
+        radius = {tl: radius, tr: radius, br: radius, bl: radius};
+    } else {
+        let defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+        for (let side in defaultRadius) {
+            radius[side] = radius[side] || defaultRadius[side];
+        }
+    }
+    this.beginPath();
+    this.moveTo(x + radius.tl, y);
+    this.lineTo(x + width - radius.tr, y);
+    this.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+    this.lineTo(x + width, y + height - radius.br);
+    this.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+    this.lineTo(x + radius.bl, y + height);
+    this.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+    this.lineTo(x, y + radius.tl);
+    this.quadraticCurveTo(x, y, x + radius.tl, y);
+    this.closePath();
+}
---
+++ index.js
@@ -0,0 +1,78 @@
+//############################## Game mechanics ##############################\\
+
+const deckSize = 20;
+
+var deck;
+var hand;
+
+function initialiseDeck(){
+	deck = [];
+	hand = [];
+	for(let i = 0; i < deckSize; i++){
+		const idx = (Math.floor(Math.random() * 5));
+		deck.push(new Card("debug[" + idx + "].png"));
+	}
+	console.log(deck);
+}
+
+function shuffleDeck(){
+	for (let i = deckSize - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+}
+
+function drawACard(){
+	hand.push(deck.pop());
+}
+
+//################################## Visuals ##################################\\
+
+
+var canvas = document.getElementById("canvas");
+//Maybe add an option later to change the preferred resolution, therewith changing these variables? For now, they are consts...
+const optimalWidth = 1920;
+const optimalHeight = 1080;
+var width, height, scl;
+
+function resize(){	
+	//Make the canvas always fit the screen with a screen ratio of 9/16
+	let tw = window.innerWidth - 25;
+	let th = window.innerHeight - 25;
+	if(th < tw * 9/16){
+		height = th;
+		width = th * 16/9;
+	}else{
+		height = tw * 9/16;
+		width = tw;
+	}
+	canvas.width = width;
+	canvas.height = height;
+	scl = width / optimalWidth;
+	Card.rsz();
+	redraw();
+}
+
+function loadGame(){
+	initialiseDeck();
+	shuffleDeck();
+	for(let i = 0; i < 4; i++)drawACard();
+	resize();
+	/*let test = new Card("debug[0].png");
+	test.display(10, 10, false);*/
+}
+
+function showHand(){
+	let x = 10;
+	for(let c of hand){
+		c.display(x, height - stdH - 10);
+		x += stdW + 10;
+	}
+}
+
+function redraw(){
+	showHand();
+}
+
+window.onload = loadGame;
+window.onresize = resize;
